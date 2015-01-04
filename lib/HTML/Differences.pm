bb749dd541e800abe8b9066bad0407eb5e3e1051
Dave Rolsky <autarch@urth.org>
2015-01-04 13:49:14 -0600

    Docs for module

--- lib/HTML/Differences.pm
+++ lib/HTML/Differences.pm
@@ -146,4 +146,99 @@ sub processing_instruction {
 
 1;
 
-# ABSTRACT: Use 
+# ABSTRACT: Reasonable sane HTML diffing
+
+__END__
+
+=head1 SYNOPSIS
+
+    use HTML::Differences qw( html_text_diff );
+
+    my $html1 = <<'EOF';
+    <p>Some text</p>
+    EOF
+
+    my $html2 = <<'EOF';
+    <p>Some <strong>strong</strong> text</p>
+    EOF
+
+    print html_text_diff( $html1, $html2 );
+
+=head1 DESCRIPTION
+
+This module provides a reasonable sane way to get the diff between two HTML
+documents or fragments. Under the hood, it uses L<HTML::Parser>.
+
+=head2 How the Diffing Works
+
+Internally, this module converts the HTML it gets into an array reference
+containing each unique HTML token. These tokens consists of things such as the
+doctype declaration, tag start & end, text, etc.
+
+All whitespace between two pieces of text is converted to a single space,
+I<except> when inside a C<< <pre> >> block. Leading and trailing space on text
+is also stripped out.
+
+Start tags are normalized so that attributes appear in sorted order, and all
+quotes are converted to double quotes, with one space before each
+attribute. Self-closing tags (like C<< <hr/> >>) are converted to their
+simpler form (C<< <hr> >>).
+
+Note that because L<HTML::Parser> decodes HTML entities inside attribute
+values, this module cannot distinguish between two attributes where one
+contains an entity and one does not.
+
+Missing end tags I<are not> added, and will show up in the diff.
+
+Comments are included by default, but you can pass a flag to ignore them.
+
+=head1 IMPORTABLE SUBROUTINES
+
+This module offers two optionally importable subroutines. Nothing is exported
+by default.
+
+=head2 html_text_diff( $html1, $html2, %options )
+
+This subroutine uses L<Text::Diff>'s C<diff()> subroutine to provide a string
+version of the diff between the two pieces of HTML provided.
+
+The HTML can be passed as a plain scalar or as a reference to a scalar.
+
+After the two HTML parameters, you can pass key/value pairs as options:
+
+=over 4
+
+=item * ignore_comments
+
+If this is true, then comments are ignored for the purpose of the diff. This
+defaults to false.
+
+=item * style
+
+The style for the diff. This defaults to "Table". See L<Text::Diff> for the
+available options.
+
+=item * context
+
+The amount of context to show in the diff. This defaults to C<2**31> to
+include all the context. You can set this to some smaller value if you prefer.
+
+=back
+
+=head2 diffable_html( $html1, $html2, %options )
+
+This returns an array reference of strings suitable for passing to any of
+L<Algorithm::Diff>'s methods or exported subroutines.
+
+The only option currently accepted is C<ignore_comments>.
+
+=head1 WHY THIS MODULE EXISTS
+
+There are a couple other modules out there that do HTML diffs, so why write
+this one?
+
+The L<HTML::Diff> module uses regexes to parse HTML. This is crazy.
+
+The L<Test::HTML::Differences> module attempts to fix up the HTML a little too
+much for my purposes. It ends up ignoring missing end tags or breaking on them
+in various ways.
